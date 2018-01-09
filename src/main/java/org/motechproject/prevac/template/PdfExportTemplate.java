4b6d0a0d0a11fb13f1afa1c0f8a37a30933632eb
Karol Cissewski <kcissewski@soldevelo.com>
2018-01-09 11:52:45 +0100

    EBODAC-1148: Add PDF and XLS templates

--- src/main/java/org/motechproject/prevac/template/PdfExportTemplate.java
+++ src/main/java/org/motechproject/prevac/template/PdfExportTemplate.java
@@ -6,7 +6,7 @@ import java.io.OutputStream;
 
 public class PdfExportTemplate extends PdfBasicTemplate {
 
-    private static final String TEMPLATE_PATH = "/export_template.pdf";
+    private static final String TEMPLATE_PATH = "/templates/export_template.pdf";
     private static final Rectangle FIRST_PAGE_RECTANGLE = new Rectangle(20, 36, 580, 580);
 
     public PdfExportTemplate(OutputStream outputStream) {
--- src/main/java/org/motechproject/prevac/template/XlsExportTemplate.java
+++ src/main/java/org/motechproject/prevac/template/XlsExportTemplate.java
@@ -4,7 +4,7 @@ import java.io.OutputStream;
 
 public class XlsExportTemplate extends XlsBasicTemplate {
 
-    private static final String XLS_TEMPLATE_PATH = "/export_template.xls";
+    private static final String XLS_TEMPLATE_PATH = "/templates/export_template.xls";
     private static final int INDEX_OF_HEADER_ROW = 9;
 
     public XlsExportTemplate(OutputStream outputStream) {
