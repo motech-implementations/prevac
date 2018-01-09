c9af9713c7ddff1cdd0b89fb0a2f413eb78114db
Karol Cissewski <kcissewski@soldevelo.com>
2018-01-09 14:44:12 +0100

    EBODAC-1155: Change positions of VisitScheduleOffset fields

--- src/main/java/org/motechproject/prevac/domain/VisitScheduleOffset.java
+++ src/main/java/org/motechproject/prevac/domain/VisitScheduleOffset.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.motechproject.mds.annotations.Entity;
 import org.motechproject.mds.annotations.Field;
 import org.motechproject.mds.annotations.NonEditable;
+import org.motechproject.mds.annotations.UIDisplayable;
 import org.motechproject.prevac.domain.enums.VisitType;
 
 import javax.jdo.annotations.Unique;
@@ -13,21 +14,25 @@ import javax.jdo.annotations.Unique;
 @Unique(name = "visitType", members = { "visitType"})
 public class VisitScheduleOffset {
 
+    @UIDisplayable(position = 0)
     @Field(required = true)
     @Getter
     @Setter
     private VisitType visitType;
 
+    @UIDisplayable(position = 1)
     @Field(required = true)
     @Getter
     @Setter
     private Integer timeOffset;
 
+    @UIDisplayable(position = 2)
     @Field(required = true)
     @Getter
     @Setter
     private Integer earliestDateOffset;
 
+    @UIDisplayable(position = 3)
     @Field(required = true)
     @Getter
     @Setter
