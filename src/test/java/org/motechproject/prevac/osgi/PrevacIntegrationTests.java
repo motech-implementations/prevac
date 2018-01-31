63b256f578fee4a235ab74ab7e6f20fe6283a6bf
Karol Cissewski <kcissewski@soldevelo.com>
2018-01-31 14:09:57 +0100

    EBODAC-1168: Copy PrevacWebIT

---
+++ src/test/java/org/motechproject/prevac/osgi/PrevacIntegrationTests.java
@@ -0,0 +1,14 @@
+package org.motechproject.prevac.osgi;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Prevac bundle integration tests suite.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        PrevacWebIT.class
+})
+public class PrevacIntegrationTests {
+}
