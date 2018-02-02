3988b9e79e3db1902c3b89ad92f2db226d00388a
Karol Cissewski <kcissewski@soldevelo.com>
2018-02-02 11:13:37 +0100

    EBODAC-1170: No space between Prevac Logo and table in PDF

--- src/main/java/org/motechproject/prevac/template/PdfExportTemplate.java
+++ src/main/java/org/motechproject/prevac/template/PdfExportTemplate.java
@@ -7,7 +7,7 @@ import java.io.OutputStream;
 public class PdfExportTemplate extends PdfBasicTemplate {
 
     private static final String TEMPLATE_PATH = "/templates/export_template.pdf";
-    private static final Rectangle FIRST_PAGE_RECTANGLE = new Rectangle(20, 36, 580, 580);
+    private static final Rectangle FIRST_PAGE_RECTANGLE = new Rectangle(20, 36, 580, 680);
 
     public PdfExportTemplate(OutputStream outputStream) {
         super(TEMPLATE_PATH, FIRST_PAGE_RECTANGLE, outputStream);
