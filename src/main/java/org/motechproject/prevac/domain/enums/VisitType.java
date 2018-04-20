3f493b69c545c32e0a2a694f485a320906f205d9
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-04-20 14:36:01 +0200

    EBODAC-1201: Change display name of some visits

--- src/main/java/org/motechproject/prevac/domain/enums/VisitType.java
+++ src/main/java/org/motechproject/prevac/domain/enums/VisitType.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 public enum VisitType {
     SCREENING("Screening"),
-    PRIME_VACCINATION_DAY("Prime Vaccination Day"),
-    PRIME_VACCINATION_FIRST_FOLLOW_UP_VISIT("Prime Vaccination First Follow-up Visit"),
-    PRIME_VACCINATION_SECOND_FOLLOW_UP_VISIT("Prime Vaccination Second Follow-up Visit"),
-    PRIME_VACCINATION_THIRD_FOLLOW_UP_VISIT("Prime Vaccination Third Follow-up Visit"),
-    BOOST_VACCINATION_DAY("Boost Vaccination Day"),
-    BOOST_VACCINATION_FIRST_FOLLOW_UP_VISIT("Boost Vaccination First Follow-up Visit"),
+    PRIME_VACCINATION_DAY("D0 Prime Vaccination Day"),
+    PRIME_VACCINATION_FIRST_FOLLOW_UP_VISIT("D7 Prime Vaccination First Follow-up Visit"),
+    PRIME_VACCINATION_SECOND_FOLLOW_UP_VISIT("D14 Prime Vaccination Second Follow-up Visit"),
+    PRIME_VACCINATION_THIRD_FOLLOW_UP_VISIT("D28 Prime Vaccination Third Follow-up Visit"),
+    BOOST_VACCINATION_DAY("D56 Boost Vaccination Day"),
+    BOOST_VACCINATION_FIRST_FOLLOW_UP_VISIT("D63 Boost Vaccination First Follow-up Visit"),
     THREE_MONTHS_POST_PRIME_VISIT("3 Months Post Prime Visit"),
     SIX_MONTHS_POST_PRIME_VISIT("6 Months Post Prime Visit"),
     TWELVE_MONTHS_POST_PRIME_VISIT("12 Months Post Prime Visit"),
